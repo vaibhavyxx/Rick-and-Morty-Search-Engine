3f0922a4b50230fd4ab7b67bf94fef934a279d33
vaibhavyxx <vd5443@rit.edu>
2025-02-15 22:22:28 -0500

    can filter searches

--- src/main.js
+++ src/main.js
@@ -1,19 +1,61 @@
 const results = document.querySelector('#results');
-
 //make a function that trims and gives you value from the input text
 //then, call the api with that name to see if it gives what you want
 
+//As of now, assuming user only adds one name
+const submitButton = document.querySelector('#search-button').addEventListener('click', ()=>{
+    let input = document.querySelector('#search-text');
+    let status = getCheckedValues("status-options");
+    let gender = getCheckedValues('gender-options');
+    let name = input.value;
 
-//gives you trimmed value of the input 
-function getString(){
-    const input = document.querySelector('#search-text');
-    let searched = input.value().trim();
-    let letters = searched.split(' ');
-    //converts the first letter to capital
-    letters[0] = letters[0].toUpperCase();
-    let secondCapitalIndex = letters.indexOf(" ");
-    letters[secondCapitalIndex + 1] = letters[secondCapitalIndex + 1].toUpperCase();
-    return letters.toString();
+    //status = document.querySelector('#status')
+    let searched = input.value.trim();
+    let fixed =  capitalizeFirstLetter(searched);
+    getData(name, status, gender);
+});
+
+//gives checked values from multiple choices
+function getCheckedValues(name){
+    let checkedBoxes = document.getElementsByName(name);
+    let result= [];
+    for(let index =0; index < checkedBoxes.length; index++){
+        if(checkedBoxes[index].checked){
+            result.push(checkedBoxes[index].value);
+        }
+    }
+    //const checkedBoxes = document.querySelector(`input[name="${name}]:checked`);
+    //const values = Array.from(checkedBoxes).map(checkbox => checkbox.value);
+    return result;
 }
 
+//capitalizes first letter
+function capitalizeFirstLetter(word){
+    return word.replace(/^./, char => char.toUpperCase());
+};
+
 //open the request to get the results for that item
+//assuming user selects only one of the options
+async function  getData(name, status, gender) {
+    let searchedURL;
+    const url="https://rickandmortyapi.com/api/character";
+    if(name!= undefined){
+        searchedURL = `${url}/?name=${name}`;
+    }
+    if(status != undefined){
+        searchedURL+= `&&status=${status.pop()}`;
+    }
+    if(gender != undefined){
+        searchedURL += `&&gender=${gender.pop()}`;
+    }
+    try{
+        const response = await fetch(searchedURL);
+        if(!response.ok){
+            throw new Error(`Response Status: ${response.status}`);
+        }
+        const json = await response.json();
+        console.log(json);
+    } catch (error){
+        console.log(error.message);
+    }
+}
